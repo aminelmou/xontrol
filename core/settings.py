7eab360b8eba2aafbb5cb1053f3cdb7f8a567561
Amin <mraminelmourabit@gmail.com>
2025-10-09 11:20:59 +0100

    back to debug is false

--- core/settings.py
+++ core/settings.py
@@ -16,12 +16,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
