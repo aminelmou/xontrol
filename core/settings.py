f557c551298e6a4b208527f75391588e326dc422
Amin <mraminelmourabit@gmail.com>
2025-10-09 09:34:22 +0100

    stop using generate random key

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # Application definition
 
